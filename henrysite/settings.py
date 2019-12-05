72bfe5d7c38b0882634d6b4f2790c3ab6ef211ca
Henry Cheng <account048924@gmail.com>
2019-12-05 13:47:51 +0800

    all

--- henrysite/settings.py
+++ henrysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '%bzl6ne)e&uuc67uw*c8%nfj#x891e^$lle-u4n7$bae%s11ep'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
